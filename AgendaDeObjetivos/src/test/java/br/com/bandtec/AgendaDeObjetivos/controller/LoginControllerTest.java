88b9cecd5142362cec8d0277461a73aecc207282
Gabriela Delfino <gabriela.delfino@outlook.com.br>
2019-09-23 21:23:43 -0300

    aula8B

--- AgendaDeObjetivos/src/test/java/br/com/bandtec/AgendaDeObjetivos/controller/LoginControllerTest.java
+++ AgendaDeObjetivos/src/test/java/br/com/bandtec/AgendaDeObjetivos/controller/LoginControllerTest.java
@@ -4,32 +4,39 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import br.com.bandtec.AgendaDeObjetivos.domain.Credenciais;
+import br.com.bandtec.AgendaDeObjetivos.domain.TodosUsuarios;
+import br.com.bandtec.AgendaDeObjetivos.domain.Usuario;
 
 public class LoginControllerTest {
-	
-	LoginController controller;		
-	
+
+	private LoginController controller;
+	private TodosUsuarios todosUsuarios;
+
 	@Before
 	public void setUp() {
-//		controller = new LoginController();		
+		todosUsuarios = Mockito.mock(TodosUsuarios.class);
+		controller = new LoginController(todosUsuarios);
 	}
-	
+
 	@Test
-	public void loginComSucesso() {		
-//		ResponseEntity<String> resposta = controller.validarLogin(new Credenciais("teste", "teste"));		
-//		assertEquals(HttpStatus.OK, resposta.getStatusCode());
-//		assertEquals("Sucesso", resposta.getBody());
+	public void loginComSucesso() {
+		Credenciais c = new Credenciais("igual", "igual");
+		Mockito.when(todosUsuarios.existe(c)).thenReturn(new Usuario());
+		ResponseEntity<String> resposta = controller.loginUsuario(c);
+		assertEquals(HttpStatus.OK, resposta.getStatusCode());
+		assertEquals("Sucesso", resposta.getBody());
 	}
-	
+
 	@Test
 	public void loginComFalha() {
-//		ResponseEntity<String> resposta = controller.validarLogin(new Credenciais("login", "senha"));
-//		assertEquals(HttpStatus.UNAUTHORIZED, resposta.getStatusCode());
-//		assertEquals("Erro", resposta.getBody());
+		ResponseEntity<String> resposta = controller.loginUsuario(c)(new Credenciais("login", "senha"));
+		assertEquals(HttpStatus.UNAUTHORIZED, resposta.getStatusCode());
+		assertEquals("Erro", resposta.getBody());
 	}
-	
+
 }
